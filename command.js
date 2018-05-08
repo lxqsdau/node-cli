389a8d47d6486f0748480181571149ab37f154db
LXuanQing <wb-lxq302621@alibaba-inc.com>
2018-05-08 22:03:11 +0800

    fr

--- command.js
+++ command.js
@@ -2,6 +2,8 @@
 console.log('liu-test-cli脚手架');
 const commander = require('commander');
 const cmd = require('node-cmd');
+const download = require('download-git-repo');
+
 // Options:
 // 能够初始化自定义的参数对象，设置关键字和描述，同时还可以设置读取用户输入的参数
 commander.version(require('./package.json').version)
@@ -16,11 +18,40 @@ commander
     .action((extensionId) => {
         id = extensionId;
         console.log(`init Extension Project "${extensionId}"`);
+        cmd.get(
+            `
+                mkdir tmp
+            `, (err, data) => {
+                console.log(err, data);
+                if (!err) {
+                    download('lxqsdau/node-cli/', 'tmp', (err) => {
+                        console.log(err)
+                    })
+                }
+            }
+        )
+        
         // todo something you need
+        /*
         cmd.get('ls', (err, data) => {
             // console.log(err, data); // null 'LICENSE\nREADME.md\ncommand.js\n'
             console.log('the current dir contains these files :\n', data)
         })
+        */
+        // cmd.get(
+        //     `
+        //         mkdir tmp
+        //         mkdir tmp/source-file
+        //         touch tmp/source-file/index.css
+        //         curl -o tpm/command.js https://github.com/lxqsdau/node-cli/command.js
+        //     `, (err, data) => {
+        //         console.log(err, data)
+        //         if (!err) {
+        //             console.log('init success');
+        //             return;
+        //         }
+        //     }
+        // )
     });
 commander.parse(process.argv); // 执行这个才能有效果
 // 命令liu-test-cli -a || liu-test-cli --aaa
@@ -39,3 +70,10 @@ if (commander.bbb) {
 if (commander.ccc) {
     console.log('ccc', commander.ccc); // 没有name输出：ccc true
 }
+/***
+ * mkdir -p
+ * mkdir -p a/b  如果a目录不存在，直接执行mkdir a/b 会报错，而加上-p，a不存在则先创建a目录，再创建下面的b目录
+ * curl
+ * curl https://wwww.baidu.com 下载单个文件，输出到shell
+ * curl -o tmp/index.html https://wwww.baidu.com  下载文件到指定目录
+ */
